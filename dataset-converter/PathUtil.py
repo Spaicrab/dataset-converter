141db67dc5177b9047370d3fc3555a7b7657c59b
Spaicrab <giuliodf2005@gmail.com>
2023-06-21 23:07:37 +0200

    Added pdm

--- dataset-converter/PathUtil.py
+++ dataset-converter/PathUtil.py
@@ -1,11 +1,14 @@
 from os.path import exists
 
+
 def fixPath(path):
-    return path.replace('\\', '/')
+    return path.replace("\\", "/")
+
 
 def changeExt(path, newExt):
     oldExt = path.split(".")[-1]
     return path.replace(f".{oldExt}", f".{newExt}")
+
 
 def findByExtList(path, extList):
     for ext in extList:
@@ -13,9 +16,11 @@ def findByExtList(path, extList):
         if exists(test):
             return test
 
+
 def extractExt(path):
     return path.split(".")[-1]
 
+
 def changeTargetFile(path, target):
     oldFile = path.split("/")[-1]
-    return path[0:len(path) - len(oldFile)] + target
+    return path[0 : len(path) - len(oldFile)] + target
